9cad4640ab4cf55c1d664cb7fdd2c5b3467d2456
Theyellowhat06 <42085659+Theyellowhat06@users.noreply.github.com>
2022-12-04 20:33:14 +0800

    desktop

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -51,7 +51,7 @@ router.get('/schedule', (req, res) => {
                     
                 var sql = `select name_, subject_code, (select number_ from class where id = class_id) as class_number, part_time, if(type_ = 1, 'Лекц', 'Лаб') as type_ 
                 from users_pt as up cross join part_time as pt cross join subjects as s 
-                where up.part_time_id = pt.id and pt.subject_code = s.code_ and up.user_code = '${req.query.teacher_id}' and pt.part_time = ${day};`
+                where up.part_time_id = pt.id and pt.subject_code = s.code_ and up.user_code = '${req.query.teacher_id}' and pt.week_day = ${day};`
                 con.query(sql, (err, rs, fields) => {
                     var subjects = [];
                     const rows = new Promise((resolve, rejects) => {
@@ -182,6 +182,111 @@ router.get('/class-attendance', (req, res) => {
                         AND semester_week = ${ss.escape(req.query.semester_week)}
                 ORDER BY created_at DESC
                 LIMIT 1), '%H:%i:%s') AS created_at
+        FROM
+            part_time pt
+                CROSS JOIN
+            users_pt upt
+                CROSS JOIN
+            users u
+        WHERE
+            pt.id = upt.part_time_id
+                AND upt.user_code = u.code_
+                AND pt.subject_code = ${ss.escape(req.query.subject_id)}
+                AND pt.week_day = ${ss.escape(req.query.week_day)}
+                AND pt.part_time = ${ss.escape(req.query.part_time)}
+                AND u.permission = 4
+                AND pt.class_id = (SELECT 
+                    class_id
+                FROM
+                    part_time ipt
+                        CROSS JOIN
+                    users_pt iupt
+                WHERE
+                    ipt.id = iupt.part_time_id
+                        AND ipt.week_day = pt.week_day
+                        AND ipt.part_time = pt.part_time
+                        AND iupt.user_code = ${ss.escape(req.query.teacher_id)});`
+            con.query(sql, (err, rs, fields) => {
+                var attendance = [];
+                var total_students = rs.length
+                var total_absent = 0
+                var total_present = 0
+                var total_sick = 0
+                var total_free = 0
+                const rows = new Promise((resolve, rejects) => {
+                    rs.forEach((element, ind, array) =>{
+                        if(element.created_at == null){
+                            var stts = 0
+                            if(element.status_ != null){
+                                stts = element.status
+                            }
+                            if(stts == 0) total_absent++;
+                            if(stts == 1) total_present++;
+                            if(stts == 2) total_sick++;
+                            if(stts == 3) total_free++;
+                            attendance.push({
+                                student_id: element.user_code,
+                                student_lname: element.lname,
+                                student_fname: element.fname,
+                                status: stts,
+                                time: element.created_at,
+                                updated_at: "19:20:45"
+                            })
+                        }else{
+                            total_present++;
+                            attendance.push({
+                                student_id: element.user_code,
+                                student_lname: element.lname,
+                                student_fname: element.fname,
+                                status: 1,
+                                time: element.created_at,
+                                updated_at: ""
+                            })
+                        }
+                        if(ind === array.length - 1) resolve()
+                    })
+                })
+                rows.then(() => {
+                    res.status(200).json({
+                        success: true,
+                        data: subjects
+                    })
+                })
+
+                res.status(200).json({
+                        success: true,
+                        data: {
+                            total_students: total_students,
+                            total_absent: total_absent,
+                            total_present: total_present,
+                            total_sick: total_sick,
+                            total_free: total_free,
+                            attendance: attendance
+                        }
+                    })
+            })
+        }catch(e){
+            console.error(e)
+            res.status(401).json({
+                success: false,
+                msg: "Permission denied"
+            })
+        }
+    }
+})
+
+router.get('/class-attendance-semester-report', (req, res) => {
+    if(!('authorization' in req.headers)){
+        res.json({
+            success: false,
+            msg: "Хандах эрхгүй байна"
+        })
+    }else{
+        try{
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, key)
+            var sql = `SELECT 
+            *
         FROM
             part_time pt
                 CROSS JOIN
